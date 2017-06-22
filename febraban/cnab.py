7ab87384dd200d67df9445e33d78f432a8f136ff
Luis Felipe Mileo <mileo@kmee.com.br>
2017-06-21 23:07:16 -0300

    [REF][cnab] Removendo código legado e migrando funções auxiliares

--- febraban/cnab.py
+++ febraban/cnab.py
@@ -20,7 +20,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-# TODO: implement abc factory?
+
+import time
+import re
+import string
 
 
 class Cnab(object):
@@ -29,21 +32,49 @@ class Cnab(object):
         pass
 
     @staticmethod
-    def get_cnab(bank, cnab_type='240'):
-        if cnab_type == '240':
+    def get_cnab(banco, layout='240'):
+        if layout == '240':
             from .cnab_240.cnab_240 import Cnab240
-            return Cnab240.get_bank(bank)
-        elif cnab_type == '400':
+            return Cnab240.get_bank(banco)
+        elif layout == '400':
             from .cnab_400.cnab_400 import Cnab400
-            return Cnab400.get_bank(bank)
-        elif cnab_type == '500':
+            return Cnab400.get_bank(banco)
+        elif layout == '500':
             from .pag_for.pag_for500 import PagFor500
-            return PagFor500.get_bank(bank)
+            return PagFor500.get_bank(banco)
         else:
             return False
 
-    def remessa(self, order):
+    def remessa(self, header, lista_boletos):
         return False
 
     def retorno(self, cnab_file):
         return object
+
+    def data_hoje(self):
+        return (int(time.strftime("%d%m%Y")))
+
+    def hora_agora(self):
+        return (int(time.strftime("%H%M%S")))
+
+    def cep(self, format):
+        sulfixo = format[-3:]
+        prefixo = format[:5]
+        return prefixo, sulfixo
+
+    @staticmethod
+    def punctuation_rm(string_value):
+        tmp_value = (
+            re.sub('[%s]' % re.escape(string.punctuation),
+                   '', string_value or ''))
+        return tmp_value
+
+    def inscricao_tipo(self, sacado_documento):
+        # TODO: Implementar codigo para PIS/PASEP
+        if len(self.punctuation_rm(sacado_documento)) > 11:
+            return 1
+        else:
+            return 2
+
+    def format_date(self, srt_date):
+        return int(srt_date.strftime('%d%m%Y'))
